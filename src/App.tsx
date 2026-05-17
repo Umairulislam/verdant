88a3501163812fb1ff97dad3b6c4eb589ee58b95
Umairulislam <umairulislam09@gmail.com>
2026-05-17 16:08:52 +0500

    chore: initialize project with Vite, Tailwind v4, and Shadcn UI

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <main>
+      <h1>Verdant Plant App</h1>
+      <Button>Click Me</Button>
+    </main>
+  )
+}
+
+export default App
